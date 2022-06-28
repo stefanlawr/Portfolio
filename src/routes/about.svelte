fdbe8f06c4e0b0fc82872772843149d94e150e51
Andrew Daniels <danielsuandrew@gmail.com>
2022-06-28 09:35:18 -0600

    Basic About page

---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<img src="./me.JPG" alt="me">
+<h1>About Me</h1>
+<p>My name is Andrew Urban Daniels.</p>
+<p>I am from Phoenix, Arizona, but currently based out of Salt Lake City, Utah. I will graduate with a Bachelors degree in Psychology from Utah Valley University in the spring of 2023. My two passions are serving others and creating memorable products. This brought forth my aspiration to learn to code. My Great Great Grandfather, Archie U. Daniels, has passed on the initials A.U.D for generations. I am the fifth generation to inherit these initials, hence the roman numeral V. </p>
+
+
+<style>
+    a {
+        color:white;
+        text-decoration: none;
+    }
+    a:hover {
+        color: rgb(144, 219, 244);
+    }
+    img {
+        width:300px;
+        margin-top:50px;
+    }
+</style>
