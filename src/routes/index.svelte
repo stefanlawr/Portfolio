7cf32070b7bb41db1eefbaa04a6ccaf19fb11342
Andrew Daniels <danielsuandrew@gmail.com>
2022-06-27 22:03:13 -0600

    removed V

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <img src="./audv.png" alt="AUDV LOGO">
-<h1>Andrew Urban Daniels V</h1>
+<h1>Andrew Urban Daniels</h1>
 <p>Welcome to my webiste.</p>
 <p>I developed this website using <a href="https://kit.svelte.dev/">SvelteKit</a>. I specialize in web development and currently I am learning JavaScript. Please feel free to look around to see projects I have been a part of, more about me, and contact information.</p>
 
